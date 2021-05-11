46ee376a3aa2e9e3578513b474bedfba4d92a868
politecat314 <55146802+politecat314@users.noreply.github.com>
2021-05-11 14:04:29 +0500

    stopword list added
    Long stopwordlist given in project description is used

--- readWordLists.py
+++ readWordLists.py
@@ -6,3 +6,6 @@ with open("./wordlists/positiveWords.txt", encoding='utf-8') as f:
     
 with open("./wordlists/negativeWords.txt", encoding='ISO-8859-1') as f:
     negativeWordList = f.readlines()[0].split(",")
+    
+with open("./wordlists/stopWordsLong.txt", encoding='utf-8') as f:
+    stopWordsLong = f.readlines()[0].split(",")
