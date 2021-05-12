cd57a852f0061e773caac89ebdb6a14d92b682c0
sameer <sameergori768@gmail.com>
2021-05-12 13:06:41 +0530

    count frequency

--- Count_Frequency.py
+++ Count_Frequency.py
@@ -1,1 +1,30 @@
-print("Hello")
+
+
+# Open the file in read mode
+text = open("sample.txt", "r")
+
+# Create an empty dictionary
+d = []
+word = ''
+words = ''
+# Loop through each line of the file
+for line in text:
+    # Remove the leading spaces and newline character
+    line = line.strip()
+
+    # Convert the characters in line to
+    # lowercase to avoid case mismatch
+    line = line.lower()
+
+    # adding all the lines into one string
+    word += line+" "
+
+# split the line into words
+words = word.split()
+#counts the words
+d = [words.count(w) for w in words]
+# converts into dic with key as name of the word and values as frequency
+Word_frequency = dict(zip(words,d))
+
+print(Word_frequency)
+
